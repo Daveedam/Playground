ab09d2e1a15e970840b39d07416c424fd764620f
David Chuguev <4davi99@gmail.com>
2017-09-11 22:49:40 +0300

    Gallons to Litters

---
+++ GalToLit.java
@@ -0,0 +1,14 @@
+package Playground;
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gall;
+
+        double litters;
+
+        gall = 20;
+        litters = gall * 3.7854;
+
+        System.out.println(gall + " галонам соответсвует " + litters + " литров");
+    }
+}
